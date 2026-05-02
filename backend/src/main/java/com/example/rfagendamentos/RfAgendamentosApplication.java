ba195090c44062d45e9d9a7d8e2ed6f59a6a7adf
Giovana <giovananaunifil@edu.unifil.br>
2026-05-02 17:06:17 -0300

    primeiro commit

---
+++ backend/src/main/java/com/example/rfagendamentos/RfAgendamentosApplication.java
@@ -0,0 +1,13 @@
+package com.example.rfagendamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfAgendamentosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RfAgendamentosApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/rfagendamentos/RfAgendamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rfagendamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfAgendamentosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
